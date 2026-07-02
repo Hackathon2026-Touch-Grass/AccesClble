b9953ada62e3d5cf085b6e225d205aabe366e177
Twanvandeneijkel <eijk0050@hz.nl>
2026-07-02 13:22:39 +0200

    added report: git difference, from latest commit and the commit before that

--- .github/workflows/CiCheck.ts
+++ .github/workflows/CiCheck.ts
@@ -2,6 +2,7 @@ import process from "node:process";
 import AltTextCheck from "./AltTextCheck.ts";
 import PhpstanCheck from "./PhpstanCheck.ts";
 import AxeCoreCheck from "./AxeCoreCheck.ts";
+import GitDiffCheck from "./GitDiffCheck.ts";
 
 const errors: string[] = [];
 
@@ -11,10 +12,18 @@ const checks = [
     { name: "Axe-core (WCAG)", check: new AxeCoreCheck() },
 ];
 
+const reports = [
+    { name: "Accessibility Diff", report: new GitDiffCheck(false) },
+];
+
 for (const ciCheck of checks) {
     if (!ciCheck.check.check()) {
         errors.push(ciCheck.name);
     }
+}
+
+for (const ciReport of reports) {
+    ciReport.report.check();
 }
 
 if (errors.length > 0) {
